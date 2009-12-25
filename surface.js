6587c15eba1d0015c71f93859395b20dc559a52a
Dan Lecocq <dlecocq@dan-lecocqs-macbook.local>
2009-12-25 14:54:08 +0900

    Added documentation to surface.js

--- surface.js
+++ surface.js
@@ -1,24 +1,62 @@
-// This class will encapsulate scalar fields
+/* This class encapsulates the surface primitive.
+ * 
+ * It requires the OpenGL context to be passed in, though 
+ * this is an incredibly ugly interface, and hopefully I 
+ * will find a clean way to work around it at some point.
+ *
+ * It also accepts a string version of the function to be
+ * plotted.  It must be GLSL 1.0-compliant string version
+ * of the function.  Parameters available are x, y, and t
+ * representing the x and y coordinates, as well as a time
+ * parameter.
+ *
+ * Currently options is not used, but eventually it will
+ * include support for what coordinate space this function
+ * is defined in, and so forth.
+ */
 function surface(context, string, options) {
 	
 	this.gl   = context;
 	this.f    = string;
 	
+	/* This is one way in which the WebGL implementation of OpenGLot
+	 * differs greatly from the C++ implementatiln.  WebGL (OpenGL 
+	 * ES 2.0) does not support display lists, and instead I've moved
+	 * the implementation to use vertex-buffer objects.  These are
+	 * those.
+	 */
 	this.vertexVBO	= null;
 	this.textureVBO = null;
 	this.indexVBO		= null;
 	
+	/* A more apt name might be "resolution," as count is the number
+	 * of samples along each axis (x and y) samples are taken. Being
+	 * set to 100 means that it will produce 2 * 100 * 100 triangles.
+	 */
 	this.count			= 100;
 
+	/* This will likely be depricated, but it currently is hidden from
+	 * the end programmer.
+	 */
 	this.initialize = function(scr) {
 		this.refresh(scr);
 		this.gen_program();
 	}
 	
+	/* Refresh is a way for the grapher instance to notify surface of
+	 * changes to the viewing environment.  All the new information is
+	 * contained in the screen object passed in, including the minimum
+	 * and maximum x and y values for the surface. In the 3D implemen-
+	 * tation, it's not commonly-used.
+	 */
 	this.refresh = function(scr) {
 		this.gen_vbo(scr);
 	}
 
+	/* All primitives are responsible for knowing how to construct them-
+	 * selves and so this is the function that constructs the VBO for
+	 * the objects.
+	 */
 	this.gen_vbo = function(scr) {
 		var vertices = [];
 		var indices  = [];
@@ -60,7 +98,13 @@ function surface(context, string, options) {
 			x += dx;
 		}
 
-		/* Add this soon */
+		/* Again, I'm not an expert in JavaScript, and I'm currently not
+		 * sure how exactly garbage collection works.  Either way, when 
+		 * generating the VBO, it's a good idea to delete the previously-
+		 * declared VBO so that it frees up some space on the GPU.  This
+		 * will be added soon, when I can find a tool that helps me track
+		 * and make sure that this memory is getting cleaned up.
+		 */
 		/*
 		if (this.vertexVBO) {
 			this.gl.console.log("deleting");
@@ -72,7 +116,11 @@ function surface(context, string, options) {
 		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexVBO);
 		this.gl.bufferData(this.gl.ARRAY_BUFFER, new WebGLFloatArray(vertices), this.gl.STATIC_DRAW);
 
-		/* Support this later
+		/* One of the options (currently anticipated from this version) is
+		 * to color the surface with a normal map or a regular texture and
+		 * lighting information for the perception of depth on the object.
+		 */
+		/*
 		this.textureVBO = this.gl.createBuffer();
 		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.textureVBO);
 		this.gl.bufferData(this.gl.ARRAY_BUFFER, new WebGLFloatArray(texture), this.gl.STATIC_DRAW);
@@ -84,13 +132,20 @@ function surface(context, string, options) {
 		this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new WebGLUnsignedShortArray(indices), this.gl.STATIC_DRAW);
 	}
 	
+	/* Every primitive is also responsible for knowing how to draw itself,
+	 * and that behavior is encapsulated in this function. It should be 
+	 * completely self-contained, returning the context state to what it
+	 * was before it's called.
+	 */
 	this.draw = function() {
 		this.gl.enableVertexAttribArray(0);
+		// This is used for texture support
 		//this.gl.enableVertexAttribArray(1);
 		
 		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexVBO);
 		this.gl.vertexAttribPointer(0, 2, this.gl.FLOAT, this.gl.FALSE, 0, 0);
 		
+		// More texture support
 		/*
 		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.textureVBO);
 		this.gl.vertexAttribPointer(1, 2, this.gl.FLOAT, this.gl.FALSE, 0, 0);
@@ -98,12 +153,24 @@ function surface(context, string, options) {
 		
 		this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.indexVBO);
 		
+		/* The number six here is magic, and relates to the number of index
+		 * references based on this.count.  Since we have this.count * this.
+		 * count * 2 triangles, not oriented in any kind of strip, it's that
+		 * value time three indices.
+		 */
 		this.gl.drawElements(this.gl.TRIANGLES, this.count * this.count * 6, this.gl.UNSIGNED_SHORT, 0);
 		
 		this.gl.disableVertexAttribArray(0);
+		
+		// Texture stuff
 		//this.gl.disableVertexAttribArray(1);
 	}
 	
+	/* Any class who inherits from the primitive class gets free access
+	 * to shader compilation and program linking, but only must provide
+	 * the fragment and vertex shader sources.  The primitive class also
+	 * provides free access to functionality for reading files.
+	 */
 	this.gen_program = function() {
 		var vertex_source = this.read("shaders/surface.vert").replace("USER_FUNCTION", this.f);
 		var frag_source		= this.read("shaders/surface.frag");
