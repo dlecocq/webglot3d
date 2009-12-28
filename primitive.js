8ec2bb80b55911304c8b0261dba1254c6ef60d9b
Dan Lecocq <dlecocq@dan-lecocqs-macbook.local>
2009-12-28 21:41:09 +0800

    Added extensive comments to the primitive class.

--- primitive.js
+++ primitive.js
@@ -1,9 +1,24 @@
+/* This is a parent class for all primitives.  Inheriting from
+ * it is not strictly necessary (as this is JavaScript), but it
+ * provides access to some very valuable functions.
+ *
+ * For example, it encapsulates reading of filenames, and shader
+ * program compilation and linking
+ */
 function primitive(context) {
 	
+	/* There is, I'm sure, a much more robust way of determining 
+	 * the best path for use as a base.  However, I've not happened
+	 * upon it yet.  Perhaps there is already a convention in place?
+	 */
 	this.BASE		 = "file:///Users/dlecocq/TTRG/webGLot3d/src/";
 	//this.BASE			 = "http://dan.lecocq.us/wordpress/webGLot3d/"
 	this.program = null;
 	
+	/* Encapsulates reading a file, usually for shader source inc-
+	 * lusion. There may be a faster and/or more robust way of doing
+	 * this, but the encapsulation makes for easy changes.
+	 */
 	this.read = function(filename) {
 		var request = new XMLHttpRequest();
 		var url = this.BASE + filename;
@@ -13,6 +28,12 @@ function primitive(context) {
 		return request.responseText;
 	}
 	
+	/* Compile AND link the shader program
+	 *
+	 * Give it vertex source and shader source, and it will populate
+	 * this.program with a shader program.  It prints out errors to 
+	 * the console, which can prove extremely helpful.
+	 */
 	this.compile_program = function(vertex_source, frag_source) {
 		var vertex_shader = this.gl.createShader(this.gl.VERTEX_SHADER);
 		var frag_shader		= this.gl.createShader(this.gl.FRAGMENT_SHADER);
@@ -24,18 +45,24 @@ function primitive(context) {
 		this.gl.compileShader(frag_shader);
 		
 		/* You can handle the compile status with the code provided by 
-		 * Khronos or every WebGL demo anywhere.
+		 * Khronos or every WebGL demo anywhere.  In fact, that's where
+		 * this code came from.
 		 */
-		//*
-		// Check the compile status
 		var compiled = this.gl.getShaderParameter(vertex_shader, this.gl.COMPILE_STATUS);
 		if (!compiled) {
 				// Something went wrong during compilation; get the error
 				var error = this.gl.getShaderInfoLog(vertex_shader);
+				this.gl.console.log("Error in compiling vertex shader: " + error);
+				return null;
+		}
+		
+		compiled = this.gl.getShaderParameter(frag_shader, this.gl.COMPILE_STATUS);
+		if (!compiled) {
+				// Something went wrong during compilation; get the error
+				var error = this.gl.getShaderInfoLog(frag_shader);
 				this.gl.console.log("Error in compiling fragment shader: " + error);
 				return null;
 		}
-		//*/
 
 		// Create the program object
 		this.program = this.gl.createProgram();
@@ -50,18 +77,21 @@ function primitive(context) {
 		/* You can check link status with this code, found at Khronos
 		 * and every WebGL demo everywhere
 		 */
-		//*
-		// Check the link status
 		var linked = this.gl.getProgramParameter(this.program, this.gl.LINK_STATUS);
 		if (!linked) {
 				// something went wrong with the link
 				var error = this.gl.getProgramInfoLog (this.program);
 				this.gl.console.log("Error in program linking:"+error);
-
-			 return null;
+				/* It's probably a best practice to delete the compiled
+				 * shaders in this case.  You should implement that.
+				 */
+				return null;
 		}
-		//*/
 		
+		/* This is actually probably a VERY BAD IDEA! Remove it at earliest
+		 * convenience, as the grapher automatically switches programs for
+		 * primitives.
+		 */
 		this.gl.useProgram(this.program);
 	}
 	
