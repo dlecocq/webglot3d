a1775360a7cf03233f20a3b4d32042447e031a82
Dan Lecocq <dlecocq@dan-lecocqs-macbook-2.local>
2010-02-05 16:12:41 +0300

    Added isosurface class. Proceeding with update.

---
+++ isosurface.js
@@ -0,0 +1,202 @@
+/* This class encapsulates the surface primitive.
+ * 
+ * It requires the OpenGL context to be passed in, though 
+ * this is an incredibly ugly interface, and hopefully I 
+ * will find a clean way to work around it at some point.
+ *
+ * It also accepts a string version of the function to be
+ * plotted.  It must be GLSL 1.0-compliant string version
+ * of the function.  Parameters available are x, y, and t
+ * representing the x and y coordinates, as well as a time
+ * parameter.
+ *
+ * Currently options is not used, but eventually it will
+ * include support for what coordinate space this function
+ * is defined in, and so forth.
+ */
+function surface(string, options, source) {
+	
+	this.gl   = null;
+	this.f    = string;
+	
+	/* This is one way in which the WebGL implementation of OpenGLot
+	 * differs greatly from the C++ implementatiln.  WebGL (OpenGL 
+	 * ES 2.0) does not support display lists, and instead I've moved
+	 * the implementation to use vertex-buffer objects.  These are
+	 * those.
+	 */
+	this.vertexVBO	= null;
+	this.textureVBO = null;
+	this.indexVBO		= null;
+	
+	/* A more apt name might be "resolution," as count is the number
+	 * of samples along each axis (x and y) samples are taken. Being
+	 * set to 100 means that it will produce 2 * 100 * 100 triangles.
+	 */
+	this.count			= 150;
+	this.index_ct   = 0;
+	
+	this.texture    = null;
+	this.source     = source || "textures/deisa.jpg";
+	//"textures/saudi-flag.gif"
+	//"textures/dan.jpg"
+
+	/* This will likely be depricated, but it currently is hidden from
+	 * the end programmer.
+	 */
+	this.initialize = function(gl, scr) {
+		this.gl = gl;
+		this.refresh(scr);
+		this.gen_program();
+	}
+	
+	/* Refresh is a way for the grapher instance to notify surface of
+	 * changes to the viewing environment.  All the new information is
+	 * contained in the screen object passed in, including the minimum
+	 * and maximum x and y values for the surface. In the 3D implemen-
+	 * tation, it's not commonly-used.
+	 */
+	this.refresh = function(scr) {
+		this.gen_vbo(scr);
+		this.texture = new texture(this.gl, this.source);
+	}
+
+	/* All primitives are responsible for knowing how to construct them-
+	 * selves and so this is the function that constructs the VBO for
+	 * the objects.
+	 */
+	this.gen_vbo = function(scr) {
+		var vertices = [];
+		var texture  = [];
+		var indices  = [];
+		
+		var texrepeat = 3;
+		
+		var x = scr.minx;
+		var y = scr.miny;
+		var dx = (scr.maxx - scr.minx) / this.count;
+		var dy = (scr.maxy - scr.miny) / this.count;
+		
+		var tx = 0.0;
+		var ty = texrepeat;
+		var dt = texrepeat / this.count;
+		
+		var i = 0;
+		var j = 0;
+		
+		/* This could probably still be optimized, but at least it's now
+		 * using a single triangle strip to render the mesh.  Much better
+		 * than the alternative.
+		 */
+		for (i = 0; i <= this.count; ++i) {
+			y = scr.miny;
+			ty = texrepeat;
+			for (j = 0; j <= this.count; ++j) {
+				vertices.push(x);
+				vertices.push(y);
+				texture.push(tx);
+				texture.push(ty);
+				
+				y += dy;
+				ty -= dt;
+			}
+			x += dx;
+			tx += dt;
+		}
+		
+		var c = 0;
+		indices.push(c)
+		
+		var inc = this.count + 1;
+		var dec = inc - 1;
+		
+		for (i = 0; i < this.count; ++i) {
+			for (j = 0; j < this.count; ++j) {
+				c += inc;
+				indices.push(c);
+				c -= dec;
+				indices.push(c);
+			}
+			c += inc;
+			indices.push(c);
+			indices.push(c);
+			
+			if (dec < inc) {
+				dec = inc + 1;
+			} else {
+				dec = inc - 1;
+			}
+		}
+
+		/* Again, I'm not an expert in JavaScript, and I'm currently not
+		 * sure how exactly garbage collection works.  Either way, when 
+		 * generating the VBO, it's a good idea to delete the previously-
+		 * declared VBO so that it frees up some space on the GPU.  This
+		 * will be added soon, when I can find a tool that helps me track
+		 * and make sure that this memory is getting cleaned up.
+		 */
+		/*
+		if (this.vertexVBO) {
+			this.gl.console.log("deleting");
+			this.gl.deleteBuffer(this.vertexVBO);
+		}
+		*/
+		
+		this.vertexVBO = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexVBO);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER, new WebGLFloatArray(vertices), this.gl.STATIC_DRAW);
+
+		this.textureVBO = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.textureVBO);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER, new WebGLFloatArray(texture), this.gl.STATIC_DRAW);
+		
+		this.indexVBO = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.indexVBO);
+		this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new WebGLUnsignedShortArray(indices), this.gl.STATIC_DRAW);
+		
+		this.index_ct = indices.length;
+	}
+	
+	/* Every primitive is also responsible for knowing how to draw itself,
+	 * and that behavior is encapsulated in this function. It should be 
+	 * completely self-contained, returning the context state to what it
+	 * was before it's called.
+	 */
+	this.draw = function() {
+		//scr.set_uniforms(this.gl, this.program);
+		this.gl.uniform1i(this.gl.getUniformLocation(this.program, "sampler"), 0);
+		
+		this.gl.enableVertexAttribArray(0);
+		this.gl.enableVertexAttribArray(1);
+		
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexVBO);
+		this.gl.vertexAttribPointer(0, 2, this.gl.FLOAT, this.gl.FALSE, 0, 0);
+		
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.textureVBO);
+		this.gl.vertexAttribPointer(1, 2, this.gl.FLOAT, this.gl.FALSE, 0, 0);
+		
+		this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.indexVBO);
+		this.texture.bind();
+		
+		this.gl.drawElements(this.gl.TRIANGLE_STRIP, this.index_ct, this.gl.UNSIGNED_SHORT, 0);
+		
+		this.gl.disableVertexAttribArray(0);
+		this.gl.disableVertexAttribArray(1);
+	}
+	
+	/* Any class who inherits from the primitive class gets free access
+	 * to shader compilation and program linking, but only must provide
+	 * the fragment and vertex shader sources.  The primitive class also
+	 * provides free access to functionality for reading files.
+	 */
+	this.gen_program = function() {
+		var vertex_source = this.read("shaders/surface.vert").replace("USER_FUNCTION", this.f);
+		var frag_source		= this.read("shaders/surface.frag");
+		
+		vertex_source = vertex_source.replace("/* CYLINDRICAL", "//* Cylindrical")
+		
+		this.compile_program(vertex_source, frag_source);		
+	}
+}
+
+surface.prototype = new primitive();
