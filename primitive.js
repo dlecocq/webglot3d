337f5cd8819da168177538f69e69709d7d00e95f
Dan Lecocq <dan.lecocq@kaust.edu.sa>
2010-05-19 17:35:26 +0300

    Fixed the integer constant to float constant regular expression.  It turns out you need to use negative lookahead to properly accomplish this.

--- primitive.js
+++ primitive.js
@@ -43,8 +43,8 @@ function primitive(context) {
 			vertex_source = vertex_source.replace("// USER_PARAMETERS", params);
 			frag_source   = frag_source.replace(  "// USER_PARAMETERS", params);
 			
-			vertex_source = vertex_source.replace(/([^\d\w\.]|^)(\d+)([^\d\w\.]|$)/g, "$1$2.0$3");
-			frag_source   = frag_source.replace(/([^\d\w\.]|^)(\d+)([^\d\w\.]|$)/g, "$1$2.0$3");
+			vertex_source = vertex_source.replace(/([^\d\w\.]|^)(\d+)(?![\w\.\d])/g, "$1$2.0");
+			frag_source   = frag_source.replace(/([^\d\w\.]|^)(\d+)(?![\w\.\d])/g, "$1$2.0");
 		}
 		
 		/*
