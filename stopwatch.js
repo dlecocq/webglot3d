d3da292e84d2c3f8317d6a6d18378052f2c9fce1
Dan Lecocq <dlecocq@dan-lecocqs-macbook.local>
2010-06-24 08:34:24 -0400

    Added JSDoc documentation to the stopwatch class.

--- stopwatch.js
+++ stopwatch.js
@@ -1,49 +1,33 @@
-/** \brief stopwatch : encapsulates timing routines
-	*
-	* This class encapsulates the system's timing functions.
-	* I've found that Mac and Linux use very different
-	* versions of time.h, and with different clock resolutions.
-	* Rather than deal with it in the silly C-style (this 
-	* is C++ after all), I created a simple stopwatch class.
-	*/
-
+/** Encapsulates timing routines.  All times are returned in 
+ * seconds.
+ *
+ * @constructor
+ */
 function stopwatch() {
-	
+	/** The start Time object */
 	this.s	 = null;
+	/** A temporary Time object for retrieving the difference
+	 * between the start and the current time */
 	this.tmp = null;
+	/** The stopped Time object */
 	this.t	 = 0;
 
-	/** \brief Start the stopwatch
-		*
-		* Start the timing of the stopwatch, just records the
-		* current WALL TIME.	This does not measure anything
-		* but the wall time.	Not system time, not clock cycles
-		* just the wall time.
-		*/
+	/** Start the stopwatch, recording the current time. */
 	this.start = function() {
 		this.s = new Date().getTime();
 	}
 
-	/** \brief Get the current time of the stopwatch
-		*
-		* This returns the stopwatch's current time without
-		* stopping it.	Returns the time in seconds as a double
-		*/
+	/** Get the current time of the stopwatch WITHOUT stopping it */
 	this.time = function() {
 		this.tmp = new Date().getTime();
 		this.t = (this.tmp - this.s) / 1000;
 		return this.t;
 	}
 
-	/** \brief Stop the stopwatch, get the time
-		*
-		* This returns the stopwatch's current time and
-		* does stop it.	 Returns the time in seconds as a double
-		*/
+	/** Stop the stopwatch, and return the time in seconds */
 	this.stop = function() {
 		this.tmp = new Date().getTime();
 		this.t = (this.tmp - this.s) / 1000;
 		return this.t;
-	}
-	
+	}	
 }
