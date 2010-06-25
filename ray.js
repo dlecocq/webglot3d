28c351f55768870e463e6e6a0e47316de4c02a00
Dan Lecocq <dlecocq@dan-lecocqs-macbook.local>
2010-06-24 22:37:35 -0400

    Added JSDoc-umentation to ray.js

--- ray.js
+++ ray.js
@@ -1,18 +1,28 @@
-/* This class is a simple encapsulation of some ray
+/** @class
+ * This class is a simple encapsulation of some ray
  * functionality.  I use it for the trackball interface
+ *
+ * In all fairness, it is a ray implementation that assumes
+ * that all rays begin at the origin.  A more appropriate
+ * name for this class might have been vector.
+ *
+ * @param {Number} x the x component 
+ * @param {Number} y the y component
+ * @param {Number} z the z component
  */
 function ray(x, y, z) {
-	
-	/* In all fairness, it is a ray implementation that assumes
-	 * that all rays begin at the origin.  A more appropriate
-	 * name for this class might have been vector.
-	 */
+	/** Local copy of the x coordinate */
 	this.x = x;
+	/** Local copy of the y coordinate */
 	this.y = y;
+	/** Local copy of the z coordinate */
 	this.z = z;
 	
-	/* Cross product.  Returns a brand new instance of ray that
+	/** Cross product.  Returns a brand new instance of ray that
 	 * is orthogonal to both this and other.
+	 * 
+	 * @param {ray} other
+	 * @returns ray
 	 */
 	this.cross = function(other) {
 		var x = this.y * other.z - this.z * other.y;
@@ -21,12 +31,19 @@ function ray(x, y, z) {
 		return new ray(x, y, z);
 	}
 	
-	// Length of the ray
+	/** Length of the ray
+	 *
+	 * @returns Number
+	 */
 	this.length = function() {
 		return Math.sqrt(x * x + y * y + z * z);
 	}
 	
-	// Dot this ray with another, called other.
+	/** Dot this ray with another, called other.
+	 *
+	 * @param {ray} other
+	 * @returns Number
+	 */
 	this.dot = function(other) {
 		return this.x * other.x + this.y * other.y + this.z * other.z;
 	}
